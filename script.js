336fab408e45ae493b3f8ea97b420e4445378b01
_1kg <108232008+1kawdalg@users.noreply.github.com>
2024-07-30 09:22:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const tg = window.Telegram.WebApp;
+
 document.getElementById('sendMessageButton').addEventListener('click', () => {
-    const receivedDataDiv = document.getElementById('receivedData');
-    receivedDataDiv.innerText = 'Hello from WebApp!';
+    const data = { message: 'Hello from WebApp!' };
+    tg.sendData(JSON.stringify(data)); // Отправка данных боту
     console.log('Send Message button clicked');
 });
 
 // Для отладки
-console.log('Standalone WebApp initialized');
+console.log('Telegram WebApp initialized', tg);
